1c661f5d6cb8cb26eefc302c0e0b142ea5fa0590
David Hwang <dthwang@gmail.com>
2013-06-02 00:41:03 +0000

    tpl adjustments for header

--- sites/all/themes/campsite/templates/page.tpl.php
+++ sites/all/themes/campsite/templates/page.tpl.php
@@ -81,12 +81,15 @@
       <?php print render($page['navigation']); ?>
     </div><!-- /#navigation -->
 
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
-    <?php endif; ?>
-
     <?php if ($site_name || $site_slogan): ?>
-      <hgroup id="name-and-slogan">
+      <hgroup id="logo-name-and-slogan">
+        <?php if ($logo): ?>
+          <div id="logo">
+          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" /></a>
+          </div>
+        <?php endif; ?>
+
+        <div class="name-and-slogan">
         <?php if ($site_name): ?>
           <h1 id="site-name">
             <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
@@ -96,6 +99,7 @@
         <?php if ($site_slogan): ?>
           <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
         <?php endif; ?>
+        </div>
       </hgroup><!-- /#name-and-slogan -->
     <?php endif; ?>
 
